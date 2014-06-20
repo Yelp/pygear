685d23915e35b8c2923f705fa5373d120b347f1c
Ross Schlaikjer <ross@yelp.com>
2014-06-20 15:58:51 -0700

    Add Job instance methods

--- job.c
+++ job.c
@@ -28,3 +28,120 @@ void Job_dealloc(pygear_JobObject* self){
 
     self->ob_type->tp_free((PyObject*)self);
 }
+
+/*
+ * Instance Methods
+ */
+
+
+/**
+ * Send data for a running job.
+ */
+static PyObject* pygear_job_send_data(pygear_JobObject* self, PyObject* args){
+    char* data;
+    unsigned data_size;
+    if (!PyArg_ParseTuple(args, "s#", &data, &data_size)){
+        return NULL;
+    }
+    return Py_BuildValue("i", gearman_job_send_data(self->g_Job, data, data_size));
+}
+
+/**
+ * Send warning for a running job.
+ */
+static PyObject* pygear_job_send_warning(pygear_JobObject* self, PyObject* args){
+    char* warning;
+    unsigned warning_size;
+    if (!PyArg_ParseTuple(args, "s#", &warning, &warning_size)){
+        return NULL;
+    }
+    return Py_BuildValue("i", gearman_job_send_warning(self->g_Job, warning, warning_size));
+}
+
+/**
+ * Send status information for a running job.
+ */
+static PyObject* pygear_job_send_status(pygear_JobObject* self, PyObject* args){
+    unsigned numerator, denominator;
+    if (!PyArg_ParseTuple(args, "II", &numerator, &denominator)){
+        return NULL;
+    }
+    return Py_BuildValue("i", gearman_job_send_status(self->g_Job, numerator, denominator));
+}
+
+/**
+ * Send result and complete status for a job.
+ */
+static PyObject* pygear_job_send_complete(pygear_JobObject* self, PyObject* args){
+    fprintf(stderr, "Called job_send_complete\n");
+    char* result;
+    unsigned result_size;
+    if (!PyArg_ParseTuple(args, "s#", &result, &result_size)){
+        return NULL;
+    }
+    return Py_BuildValue("i", gearman_job_send_complete(self->g_Job, result, result_size));
+}
+
+/**
+ * Send exception for a running job.
+ */
+static PyObject* pygear_job_send_exception(pygear_JobObject* self, PyObject* args){
+    char* exception;
+    unsigned exception_size;
+    if (!PyArg_ParseTuple(args, "s#", &exception, &exception_size)){
+        return NULL;
+    }
+    return Py_BuildValue("i", gearman_job_send_exception(self->g_Job, exception, exception_size));
+}
+
+/**
+ * Send fail status for a job.
+ */
+static PyObject* pygear_job_send_fail(pygear_JobObject* self){
+    return Py_BuildValue("i", gearman_job_send_fail(self->g_Job));
+}
+
+/**
+ * Get job handle.
+ */
+static PyObject* pygear_job_handle(pygear_JobObject* self){
+    return Py_BuildValue("s", gearman_job_handle(self->g_Job));
+}
+
+/**
+ * Get the function name associated with a job.
+ */
+static PyObject* pygear_job_function_name(pygear_JobObject* self){
+    return Py_BuildValue("s", gearman_job_function_name(self->g_Job));
+}
+
+/**
+ * Get the unique ID associated with a job.
+ */
+static PyObject* pygear_job_unique(pygear_JobObject* self){
+    return Py_BuildValue("s", gearman_job_unique(self->g_Job));
+}
+
+/**
+ * Get a the workload for a job.
+ */
+static PyObject* pygear_job_workload(pygear_JobObject* self){
+    return Py_BuildValue("s#",
+        gearman_job_workload(self->g_Job),
+        gearman_job_workload_size(self->g_Job)
+    );
+}
+
+/**
+ * Get size of the workload for a job.
+ */
+static PyObject* pygear_job_workload_size(pygear_JobObject* self){
+    return Py_BuildValue("I", gearman_job_workload_size(self->g_Job));
+}
+
+/**
+ * Get error string for a job.
+ */
+static PyObject* pygear_job_error(pygear_JobObject* self){
+    return Py_BuildValue("s", gearman_job_error(self->g_Job));
+}
--- job.h
+++ job.h
@@ -26,8 +26,74 @@ void Job_dealloc(pygear_JobObject* self);
 
 /* Method definitions */
 
+static PyObject* pygear_job_send_data(pygear_JobObject* self, PyObject* args);
+PyDoc_STRVAR(pygear_job_send_data_doc,
+"Send data for a running job.\n"
+"@param[in] data Data to send.");
+
+static PyObject* pygear_job_send_warning(pygear_JobObject* self, PyObject* args);
+PyDoc_STRVAR(pygear_job_send_warning_doc,
+" Send warning for a running job."
+"@param[in] warning Warning to send");
+
+static PyObject* pygear_job_send_status(pygear_JobObject* self, PyObject* args);
+PyDoc_STRVAR(pygear_job_send_status_doc,
+"Send status information for a running job."
+"@param[in] information Information to send");
+
+static PyObject* pygear_job_send_complete(pygear_JobObject* self, PyObject* args);
+PyDoc_STRVAR(pygear_job_send_complete_doc,
+"Send result and complete status for a job."
+"@param[in] result Result to send");
+
+static PyObject* pygear_job_send_exception(pygear_JobObject* self, PyObject* args);
+PyDoc_STRVAR(pygear_job_send_exception_doc,
+"Send exception for a running job."
+"@param[in] exception Exception to send");
+
+static PyObject* pygear_job_send_fail(pygear_JobObject* self);
+PyDoc_STRVAR(pygear_job_send_fail_doc,
+"Send fail status for a job.");
+
+static PyObject* pygear_job_handle(pygear_JobObject* self);
+PyDoc_STRVAR(pygear_job_handle_doc,
+"Get job handle.");
+
+static PyObject* pygear_job_function_name(pygear_JobObject* self);
+PyDoc_STRVAR(pygear_job_function_name_doc,
+"Get the function name associated with a job.");
+
+static PyObject* pygear_job_unique(pygear_JobObject* self);
+PyDoc_STRVAR(pygear_job_unique_doc,
+"Get a pointer to the workload for a job.");
+
+static PyObject* pygear_job_workload(pygear_JobObject* self);
+PyDoc_STRVAR(pygear_job_workload_doc,
+"Get the workload for a job.");
+
+static PyObject* pygear_job_workload_size(pygear_JobObject* self);
+PyDoc_STRVAR(pygear_job_workload_size_doc,
+"Get size of the workload for a job.");
+
+static PyObject* pygear_job_error(pygear_JobObject* self);
+PyDoc_STRVAR(pygear_job_error_doc,
+"Get a string representation of the last job error");
+
+
 /* Module method specification */
 static PyMethodDef job_module_methods[] = {
+     _JOBMETHOD(send_data,          METH_VARARGS)
+     _JOBMETHOD(send_warning,       METH_VARARGS)
+     _JOBMETHOD(send_status,        METH_VARARGS)
+     _JOBMETHOD(send_complete,      METH_VARARGS)
+     _JOBMETHOD(send_exception,     METH_VARARGS)
+     _JOBMETHOD(send_fail,          METH_NOARGS)
+     _JOBMETHOD(handle,             METH_NOARGS)
+     _JOBMETHOD(function_name,      METH_NOARGS)
+     _JOBMETHOD(unique,             METH_NOARGS)
+     _JOBMETHOD(workload,           METH_NOARGS)
+     _JOBMETHOD(workload_size,      METH_NOARGS)
+     _JOBMETHOD(error,              METH_NOARGS)
     {NULL, NULL, 0, NULL}
 };
 
