7d01f41e4422ca207421faa60bf9d14b95b29836
Ross Schlaikjer <ross@yelp.com>
2014-07-01 15:18:09 -0700

    Add base tests

---
+++ tests/test_client.py
@@ -0,0 +1,36 @@
+import pytest
+import pygear
+
+@pytest.fixture
+def c():
+    return pygear.Client()
+
+def test_execute_no_servers(c):
+    with pytest.raises(pygear.UNKNOWN_STATE):
+        c.execute("reverse", "Jackdaws love my big sphynx of quartz")
+
+def test_do_no_servers(c):
+    with pytest.raises(pygear.NO_SERVERS):
+        c.do("reverse", "Jackdaws love my big sphynx of quartz")
+
+def test_add_task_no_servers(c):
+    with pytest.raises(pygear.NO_SERVERS):
+        c.add_task("reverse", "Jackdaws love my big sphynx of quartz")
+        c.run_tasks()
+
+def test_client_timeout(c):
+    assert c.timeout() == -1
+    c.set_timeout(30)
+    assert c.timeout() == 30
+
+def test_client_add_servers(c):
+    with pytest.raises(pygear.GETADDRINFO):
+        c.add_servers("invalidhosturi")
+    with pytest.raises(pygear.GETADDRINFO):
+        c.add_server("invalidhosturi", 4730)
+
+def test_clone(c):
+    c.set_timeout(30)
+    cl = c.clone()
+    assert cl.timeout() == 30
+    assert cl.get_options() == c.get_options()
---
+++ tests/test_worker.py
@@ -0,0 +1,55 @@
+import pytest
+import pygear
+
+@pytest.fixture
+def w():
+    return pygear.Worker()
+
+def worker_rev_fn(job):
+    workload = job.workload()
+    job.send_complete(workload[::-1])
+
+def test_work_no_functions(w):
+    with pytest.raises(pygear.NO_REGISTERED_FUNCTIONS):
+        w.work()
+
+def test_work_timeout(w):
+    w.add_function("reverse", 60, worker_rev_fn)
+    w.add_server("localhost", 4730)
+    w.set_timeout(30)
+    with pytest.raises(pygear.TIMEOUT):
+        w.work()
+
+def test_worker_timeout(w):
+    assert w.timeout() == -1
+    w.set_timeout(30)
+    assert w.timeout() == 30
+
+def test_worker_function_exists(w):
+    assert w.function_exists("test_method") == False
+    w.register("test_method", 10)
+    assert w.function_exists("test_method") == True
+
+def test_worker_unregister(w):
+    assert w.function_exists("test_method") == False
+    w.register("test_method", 10)
+    assert w.function_exists("test_method") == True
+    w.unregister("test_method")
+    assert w.function_exists("test_method") == False
+
+def test_worker_unregister_all(w):
+    assert w.function_exists("test_method") == False
+    w.register("test_method", 10)
+    assert w.function_exists("test_method") == True
+    w.unregister_all()
+    assert w.function_exists("test_method") == False
+
+def test_worker_add_servers(w):
+    with pytest.raises(pygear.GETADDRINFO):
+        w.add_servers("invalidhosturi")
+
+def test_clone(w):
+    w.set_timeout(30)
+    wl = w.clone()
+    assert wl.timeout() == 30
+    assert wl.get_options() == w.get_options()
