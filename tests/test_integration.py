dbabd16d2a96ef50ea30134396b763e2b2ee36e1
Ross Schlaikjer <ross@yelp.com>
2014-08-12 10:41:42 -0700

    Simplify TestException

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -8,10 +8,8 @@ TEST_GEARMAN_SERVERS = ["srv1-devc"]
 TEST_TIMEOUT_SECONDS = 15
 
 class TestError(Exception):
-    def __init__(self, value):
-        self.value = value
-    def __str__(self):
-        return repr(self.value)
+    pass
+
 
 @pytest.fixture
 def c():
